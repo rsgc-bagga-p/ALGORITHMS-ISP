05b7d184701be7de281b1494c2d7c692d0be395d
rsgc-bagga-p <pbagga@rsgc.on.ca>
2015-12-11 10:37:40 -0500

    This is the accelerometer to serial code for the Arduino.

---
+++ Arduino_Accelerometer_to_Serial/Arduino_Accelerometer_to_Serial.ino
@@ -0,0 +1,35 @@
+/*
+ * Created by: Puneet Singh Bagga
+ * Purpose: Use an accelerometer with swift 
+ * Date: December 11th, 2015
+*/
+#include <Wire.h> //I2C library
+#include <SparkFun_MMA8452Q.h> //Includes the accelerometer library
+MMA8452Q accel;// assign the accelerometer a name
+float average[7]; //keeps the average 
+float val; //the value sent through serial
+void setup() { //loops once
+  Serial.begin(9600); //starts serial
+  accel.init(); //initializes the accelerometer
+}
+
+void loop() { //iterates forever
+
+  if (accel.available()) { //check if the accelerometer is available/working
+    accel.read(); //read accelerometer values
+    averageRead(); //prints the averaged values to serial
+  }
+} 
+
+void averageRead(){ //reads the accelerometer values and sends out an average
+ for (int i = 0; i < 7; i++){ // loops through the values and adds them to average array
+
+  average[i] = accel.cx; //assigns values to average array
+
+  val += average[i]; //adds the values to the val int 
+  
+ } 
+  val = val / 7; //averages the value
+  Serial.println(val); //prints them to serial
+}
+
