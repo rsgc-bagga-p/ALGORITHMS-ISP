afa9d2a91e375897dc1377bfb485a3ac9c7d107f
rsgc-bagga-p <pbagga@rsgc.on.ca>
2016-01-18 10:48:05 -0500

    Fix Formatting
    Fixes formating.

--- Arduino_Accelerometer_to_Serial/Arduino_Accelerometer_to_Serial.ino
+++ Arduino_Accelerometer_to_Serial/Arduino_Accelerometer_to_Serial.ino
@@ -1,12 +1,12 @@
 /*
  * Created by: Puneet Singh Bagga
- * Purpose: Use an accelerometer with swift 
+ * Purpose: Use an accelerometer with swift
  * Date: December 11th, 2015
 */
 #include <Wire.h> //I2C library
 #include <SparkFun_MMA8452Q.h> //Includes the accelerometer library
 MMA8452Q accel;// assign the accelerometer a name
-float average[15]; //keeps the average 
+float average[15]; //keeps the average
 float val; //the value sent through serial
 void setup() { //loops once
   Serial.begin(9600); //starts serial
@@ -20,19 +20,19 @@ void loop() { //iterates forever
     accel.read(); //read accelerometer values
     averageRead(); //prints the averaged values to serial
   }
-} 
+}
 
-void averageRead(){ //reads the accelerometer values and sends out an average
- for (int i = 0; i < 15; i++){ // loops through the values and adds them to average array
+void averageRead() { //reads the accelerometer values and sends out an average
+  for (int i = 0; i < 15; i++) { // loops through the values and adds them to average array
 
-  average[i] = accel.cx; //assigns values to average array
+    average[i] = accel.cx; //assigns values to average array
 
-  val += average[i]; //adds the values to the val int 
- } 
+    val += average[i]; //adds the values to the val int
+  }
   val = val / 15; //averages the value
- // float newVal = map(val, -1.00, 1.00, 0.00, 2.00); // maps the average to positive values
+  // float newVal = map(val, -1.00, 1.00, 0.00, 2.00); // maps the average to positive values
   //newVal = newVal * 250.00; // multiplies the value to canvas size
-  
+
   Serial.print(val);
   Serial.print("|");
   delay(20);
